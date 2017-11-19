cd50ae606337b6b4c08961a7c9501377143ea442
Sebastian Stokkebro <s170423.win.dtu.dk>
2017-11-19 22:28:56 +0100

    kommentar bash test

--- src/main/java/gr_34/boundary/Spillebræt.java
+++ src/main/java/gr_34/boundary/Spillebræt.java
@@ -2,7 +2,11 @@ package gr_34.boundary;
 
 import gui_fields.GUI_Board;
 import gui_fields.GUI_Field;
-
+/**
+ * Opretter spillebrættet for monopoly
+ * @author Sebastian
+ *
+ */
 public class Spillebræt {
 	public Spillebræt() {
 		GUI_Field[] felter = new GUI_Field[0];
