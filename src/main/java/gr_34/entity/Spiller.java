125b92ebe6ab0b6f7b3b0d5ad3268611080e0046
Sebastian Stokkebro <s170423.win.dtu.dk>
2017-11-20 10:04:57 +0100

    Spiller klasse oprettet

--- src/main/java/gr_34/entity/Spiller.java
+++ src/main/java/gr_34/entity/Spiller.java
@@ -1,5 +1,43 @@
 package gr_34.entity;
+/**
+ * 
+ * @author Sebastian
+ *
+ */
 
 public class Spiller {
+	private String navn;
+	private int alder;
+	private Konto konto;
+	
+	public Spiller(String navn, int alder, Konto konto) {
+		this.navn = navn;
+		this.alder = alder;
+		this.konto = konto;
+	}
+
+	public String getNavn() {
+		return navn;
+	}
+
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+
+	public int getAlder() {
+		return alder;
+	}
+
+	public void setAlder(int alder) {
+		this.alder = alder;
+	}
+
+	public Konto getKonto() {
+		return konto;
+	}
+
+	public void setKonto(Konto konto) {
+		this.konto = konto;
+	}
 
 }
