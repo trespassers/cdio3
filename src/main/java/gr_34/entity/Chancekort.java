5ede65c242e27736466223b1e255f8a5ad9a3772
petertbdtu <s175210@student.dtu.dk>
2017-11-20 10:28:47 +0100

    Tilføj tidlig chancekortklasse

--- src/main/java/gr_34/entity/Chancekort.java
+++ src/main/java/gr_34/entity/Chancekort.java
@@ -1,5 +1,27 @@
+/**
+ * Håndterer navn og beskrivende tekst af et Matador Chancekort.
+ * @author PeterTB
+ */
 package gr_34.entity;
 
 public class Chancekort {
-
+	String navn;
+	String tekst;
+	public Chancekort(String navn, String tekst) {
+		super();
+		this.navn = navn;
+		this.tekst = tekst;
+	}
+	public String getNavn() {
+		return navn;
+	}
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+	public String getTekst() {
+		return tekst;
+	}
+	public void setTekst(String tekst) {
+		this.tekst = tekst;
+	}
 }
