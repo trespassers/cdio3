6e32e8629f4dc831498a563132d21022336bdf8f
Sebastian Stokkebro <s170423.win.dtu.dk>
2017-11-21 12:51:54 +0100

    Spillerliste ændret i forhold til brik-klassen

--- src/main/java/gr_34/Main.java
+++ src/main/java/gr_34/Main.java
@@ -1,7 +1,12 @@
 package gr_34;
 
+import gr_34.entity.Spillerliste;
+
 public class Main {
 	
     public static void main( String[] args ) {
+    	Spillerliste l = new Spillerliste();
+    	l.tilføjSpiller(2);
+    	System.out.println(l.toString());
     }
 }
