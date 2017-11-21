98b20ba3de012b397d0f3ef4f044e44bfdf066c1
petertbdtu <s175210@student.dtu.dk>
2017-11-21 11:46:21 +0100

    Tilføj ChangekortBunke, igen

---
+++ src/main/java/gr_34/entity/ChancekortBunke.java
@@ -0,0 +1,22 @@
+package gr_34.entity;
+
+public class ChancekortBunke {
+	
+	private Chancekort[] bunke;
+	
+	public ChancekortBunke(Chancekort[] bunke)
+	{
+		this.bunke = bunke;
+	}
+	
+	public void bland()
+	{
+		//Bland bunken
+	}
+	public Chancekort trækKort()
+	{
+		//Tag øverste kort
+		//Læg tilbage i bunden?
+	}
+	
+}
