8280e19d82b3c6f3855e538e3f6b91767a803a6b
petertbdtu <s175210@student.dtu.dk>
2017-11-21 09:57:59 +0100

    Tilføj effekt enumerator til chancekort

--- src/main/java/gr_34/entity/Chancekort.java
+++ src/main/java/gr_34/entity/Chancekort.java
@@ -4,19 +4,25 @@
  */
 package gr_34.entity;
 
+
+
 public class Chancekort {
 	private String navn;
 	private String tekst;
+	private ChancekortEffekt effekt;
 	
-	public Chancekort(String navn, String tekst) {
-		super();
+	public Chancekort(String navn, String tekst, ChancekortEffekt effekt) {
 		this.navn = navn;
 		this.tekst = tekst;
+		this.effekt = effekt;
 	}
 	public String getNavn() {
 		return navn;
 	}
 	public String getTekst() {
 		return tekst;
+	}
+	public ChancekortEffekt getEffekt() {
+		return effekt;
 	}
 }
