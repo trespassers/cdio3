b5f771b7a444b247d33a61834b383d1f02223c3a
Alan Jafi <Alan@alanjafis-mbp.eduroam.wireless.dtu.dk>
2017-11-24 10:29:31 +0100

    brik kommentar opdatering

--- src/main/java/gr_34/entity/Brik.java
+++ src/main/java/gr_34/entity/Brik.java
@@ -6,11 +6,16 @@ import gui_fields.GUI_Car;
 import gui_fields.GUI_Car.Pattern;
 import gui_fields.GUI_Car.Type;
 
+/**
+ * @author Burhan 
+ * @author Alan
+ *
+ */
 public class Brik {
 	private GUI_Car brik;
 	private Color farve1;
 	
-	public Brik(Color farve1, Color farve2) {
+	public Brik(Color farve1, Color farve2) { //
 		this.farve1 = farve1;
 		brik = new GUI_Car(farve1, farve2, Type.CAR, Pattern.CHECKERED);
 	}
