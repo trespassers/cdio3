629e76068d863a57708892ab095c0b60bc4c25ae
SebastianStokkebro <32563781+SebastianStokkebro@users.noreply.github.com>
2017-11-25 14:56:20 +0100

    Update Main.java
    merge konflikt rettet

--- src/main/java/gr_34/Main.java
+++ src/main/java/gr_34/Main.java
@@ -1,5 +1,4 @@
 package gr_34;
-<<<<<<< HEAD
 import gr_34.controller.Controller;
 /**
  * 
@@ -7,14 +6,6 @@ import gr_34.controller.Controller;
  *
  */
 
-=======
-
-import gr_34.boundary.Spillebræt;
-/**
- * @version 0.0.5
- *
- */
->>>>>>> ed35bc973158390e4bdbec894663e6c83d56733e
 public class Main {
 	
     public static void main( String[] args ) {
